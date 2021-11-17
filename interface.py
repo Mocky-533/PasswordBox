ee1fdf68584a760ec96c7d7ce3c85ec69909e430
Mocky-533 <lnfxwyz@outlook.com>
2021-11-17 22:46:17 +0800

    switch to curses interface

---
+++ interface.py
@@ -0,0 +1,274 @@
+import curses
+from pwdmanage import DataOp
+from datetime import datetime
+import pyperclip
+
+ICON = """ *******  **       ** *******     ******     *******   **     **
+/**////**/**      /**/**////**   /*////**   **/////** //**   **
+/**   /**/**   *  /**/**    /**  /*   /**  **     //** //** **
+/******* /**  *** /**/**    /**  /******  /**      /**  //***
+/**////  /** **/**/**/**    /**  /*//// **/**      /**   **/**
+/**      /**** //****/**    **   /*    /**//**     **   ** //**
+/**      /**/   ///**/*******    /*******  //*******   **   //**
+//       //       // ///////     ///////    ///////   //     //"""
+MENU = """------------------------------
+-------------Menu-------------
+1. Create new password
+
+2. Find a password by account or email
+
+3. Find a password by site or app
+
+4. Update stored passwords
+   -> note: First find the stored password and remember its id.
+5. Delete stored password
+   -> note: same above.
+6. Change login password
+
+Q. Exit
+------------------------------"""
+
+def first_login_interface():
+    r = DataOp().register_check()
+    if len(r) == 0:
+        stdscr = curses.initscr()
+        stdscr.clear()
+        icon_win = stdscr.subwin(10, 80, 0, 5)
+        message_win = stdscr.subwin(11, 0)
+        icon_win.addstr(ICON)
+        icon_win.refresh()
+
+        message_win.addstr("First time launch, register requested.\nSET A PASSWORD FOR LOGIN\n")
+        success = False
+        while not success:
+            icon_win.touchwin()
+            curses.noecho()
+            message_win.addstr("[PASSWORD]: ")
+            login_pwd_or = message_win.getstr().decode()
+            message_win.addstr("\n[REPEATE]: ")
+            icon_win.touchwin()
+            login_pwd_or2 = message_win.getstr().decode()
+            if login_pwd_or2 == login_pwd_or:
+                success = True
+                DataOp().register(login_pwd_or)
+        curses.endwin()
+
+def login_interface():
+    stdscr = curses.initscr()
+    stdscr.clear()
+    icon_win = stdscr.subwin(10, 80, 0, 5)
+    message_win = stdscr.subwin(11, 0)
+    input_win = stdscr.subwin(16, 0)
+
+    icon_win.addstr(ICON)
+    icon_win.refresh()
+    message_win.addstr("\nVerification Required!!!\nYou have 3 chance to input the right password.")
+    message_win.addstr("\n" + 16*'#' + ' LOG IN ' + 16*'#')
+    icon_win.touchwin()
+    message_win.refresh()
+    for i in range(3):
+        curses.noecho()
+        input_win.addstr("[PASSWORD]: ")
+        icon_win.touchwin()
+        pwd = input_win.getstr().decode()
+        if DataOp().login_check(pwd):
+            input_win.clear()
+            return True
+        elif i == 2:
+            break
+        elif i == 0:
+            input_win.clear()
+            input_win.addstr(f"Wrong password!!!, 2 chances left.\n")
+        elif i == 1:
+            input_win.clear()
+            input_win.addstr(f"Last Chance!!!\n")
+    curses.endwin()
+    return False
+
+def main_menu():
+    stdscr = curses.initscr()
+    stdscr.clear()
+    display_win = curses.newwin(16, 120, 0, 0)
+    input_win = curses.newwin(13, 120, 16, 0)
+
+    display_win.addstr(MENU)
+    display_win.refresh()
+    display_win.touchwin()
+    input_win.addstr(": ")
+    com = input_win.getkey()
+    if com == "1":
+        curses.endwin()
+        add_password()
+        return True
+    elif com == "2":
+        search_by_account(input_win)
+        return True
+    elif com == "3":
+        search_by_site(input_win)
+        return True
+    elif com == "4":
+        update(input_win)
+        return True
+    elif com == "5":
+        delete_password(input_win)
+        return True
+    elif com == "6":
+        change_login_key(input_win)
+        return True
+    elif com == "q" or com == "Q":
+        input_win.addstr("\nQuit Confirm?")
+        if input_win.getch() == 10:
+            return False
+        else:
+            return True
+    else:
+        return True
+
+def add_password():
+    stdscr = curses.initscr()
+    stdscr.clear()
+    curses.echo()
+    stdscr.addstr("\n<Site or App name>: \n")
+    site = stdscr.getstr().decode()
+    if len(site) == 0:
+        return
+    stdscr.addstr("<Account or email>: \n")
+    account = stdscr.getstr().decode()
+    account_for_dup = "".join(['\\b', account, '\\b'])
+    dup = DataOp().dulplicated(site, account_for_dup)
+    if len(dup) > 0:
+        stdscr.addstr("Password(s) of this site using same account exist: \n")
+        display(dup, 0, stdscr)
+        stdscr.addstr("\nUpdate this password? ['F' to update, otherwise continue adding]: \n")
+        op = stdscr.getkey()
+        if op == 'F' or op == 'f':
+            update(stdscr)
+            return
+    stdscr.addstr("<Password>: \n")
+    curses.noecho()
+    pwd_or = stdscr.getstr().decode()
+    curses.echo()
+    stdscr.addstr("Confirm? ['Y' to confirm]: ")
+    check = stdscr.getkey()
+    if check == "y" or check == "Y":
+        now = datetime.now()
+        time = now.strftime('%Y-%m-%d %H:%M:%S')
+        col = []
+        for i in (site, account, pwd_or, time):
+            col.append(i)
+        DataOp().add(col)
+
+def search_by_account(work_win):
+    work_win.addstr("\n<Account or email>: \n")
+    curses.echo()
+    account = work_win.getstr().decode()
+    if len(account) == 0:
+        return
+    pwds = DataOp().search_account(account)
+    if len(pwds) > 0:
+        display(pwds, 1, work_win)
+        pass
+    else:
+        work_win.addstr("Nothing found.")
+        work_win.getkey()
+
+def search_by_site(work_win):
+    curses.echo()
+    work_win.addstr("\n<Site or App>: \n")
+    site = work_win.getstr().decode()
+    if len(site) == 0:
+        return
+    pwds = DataOp().search_site(site)
+    if len(pwds) > 0:
+        display(pwds, 1, work_win)
+        pass
+    else:
+        work_win.addstr("Nothing found.")
+        work_win.getkey()
+
+def update(work_win):
+    curses.echo()
+    work_win.addstr("\n<Password ID>: \n")
+    id = work_win.getstr().decode()
+    if len(id) == 0:
+        return
+    work_win.addstr("<NEW password>: \n")
+    curses.noecho()
+    pwd = work_win.getstr().decode()
+    work_win.addstr("Confirm? ['Y' to confirm]: ")
+    curses.echo()
+    check = work_win.getstr().decode()
+    if check == "y" or check == "Y":
+        now = datetime.now()
+        time = now.strftime('%Y-%m-%d %H:%M:%S')
+        col = []
+        for i in (pwd, time, id):
+            col.append(i)
+        DataOp().update_pwd(col)
+
+def delete_password(work_win):
+    work_win.addstr("\n<Password ID>: ")
+    curses.echo()
+    id = work_win.getstr().decode()
+    if len(id) == 0:
+        return
+    work_win.addstr("Confirm? ['Y' to confirm]: ")
+    check = work_win.getstr().decode()
+    if check == "y" or check == "Y":
+        DataOp().delete(id)
+
+def change_login_key(work_win):
+    work_win.addstr('\n' + 16*'#' + ' CHANGE PASSWORD ' + 16*'#' + '\n')
+    verified = False
+    success = False
+    for n in range(3):
+        work_win.addstr("[OLD PASSWORD]: ")
+        curses.noecho()
+        old_pwd = work_win.getstr().decode()
+        verified = DataOp().login_check(old_pwd)
+        if verified:
+            while not success:
+                work_win.addstr("\n[NEW PASSWORD]: ")
+                new_pwd1 = work_win.getstr().decode()
+                work_win.addstr("\n[REPEAT NEW PASSWORD]: ")
+                new_pwd2 = work_win.getstr().decode()
+                if new_pwd1 == new_pwd2:
+                    success = True
+                    DataOp().change_login(new_pwd1)
+                    return
+                else:
+                    work_win.clear()
+                    work_win.addstr(16*'#' + ' CHANGE PASSWORD ' + 16*'#' + '\n')
+                    work_win.addstr("Different input, try again!")
+        else:
+            work_win.clear()
+            work_win.addstr(16*'#' + ' CHANGE PASSWORD ' + 16*'#' + '\n')
+            work_win.addstr(f"Wrong password, {2-n} chances left.")
+
+def display(pwd_result, flag, win):
+    win.addstr(42*'#' + '  SEARCH RESULT  ' + 50*'#' + "\n" + 109*'-' + '\n')
+    win.addstr("|{:^3}|{:^15}|{:^40}|{:^30}|{:^15}|".format('ID', 'SITE', 'ACCOUNT', 'PASSWORD', 'DATE'))
+    for pwdinfo in pwd_result:
+        date = pwdinfo[4].split(" ")
+        pnt = "|{:^3}|{:^15}|{:^40}|{:^30}|{:^15}|".format(pwdinfo[0], pwdinfo[1], pwdinfo[2], pwdinfo[3], date[0])
+        win.addstr("\n" + pnt)
+    win.addstr("\n" + 109*'-' + "\n" + 109*"#" + '\n')
+    note = f'{len(pwd_result)} password(s) found.'
+    win.addstr('{:>84}'.format(note))
+    win.refresh()
+    if flag == 1:
+        win.addstr("\nInput the password ID to copy password, otherwise ignore: ")
+        curses.echo()
+        num = win.getstr().decode()
+        if num.isnumeric():
+            for pwd in pwd_result:
+                if int(num) == pwd[0]:
+                    pyperclip.copy(pwd[3])
+
+
+if __name__ == "__main__":
+    first_login_interface()
+    status = False
+    status = login_interface()
+    while status:
+        status = main_menu()
--- readme.md
+++ readme.md
@@ -7,14 +7,19 @@ On first launch, a login password(key) is required. Every time after that, just 
 ## Functions
 
 1. Add password
+    - Offer a chance to update password when the one being added is duplicated.(site and account simultaneously)
 2. Search password by either app/site name or account/email
-    - results can directly be copied into clipboard. (v1.01)
+    - Chosen password can be copied into clipboard. (v1.01)
 3. Update stored password
 4. Delete stored password
 5. change login password(key)
     - add verification before changing login key. (v1.01)
 
+## Notification
+
+- Before launching, make sure the terminal running contains at least 30 lines and 100 cols, or the curses package might run into error.
+
 ## To do
 
-- [ ] A GUI (maybe)
-- [x] Optimize searching functions
+- [x] ~~A GUI (maybe)~~ Used cursor instead.
+- [x] ~~Optimize searching functions~~ DONE.
